4ec2f17d48668eccfa1ff8e9236575cb5ef71de5
Rodrigo Lara <ereodeereigeo@gmail.com>
2015-01-31 19:56:40 -0300

    dejando extracción de datos sólamente hasta generar lista de tablas sin editar

--- ext_datos.py
+++ ext_datos.py
@@ -2,7 +2,7 @@
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
-
+import obtener as ob
 #Se crea una lista de encabezados que servirá para extraer las col
 listaEnc = ['time', 'errors', 'limiters', 'currentSP', \
 'velocitySP', 'idcSP', 'miscSP', 'busVoltage','busCurrent',\
@@ -11,43 +11,15 @@ listaEnc = ['time', 'errors', 'limiters', 'currentSP', \
 '1.9v', '3.3v', 'motorTemp', 'dspTemp', 'phaseaTemp', 'phasebTemp',\
 'phasecTemp', 'cantranserr', 'canrecerr', 'slipSpeed']
 
-file = '/home/rodrigo/ownCloud/Software/Software/WSlog_2014-11-17_15-16-21.csv'
-
 #definimos label como opción oculta para acotar los datos extraidos
-def extraerData(archivo, label=listaEnc, time=0.2, values=True):
+def extraerData(archivo, fecha=None, label=listaEnc, time=0.2, values=True):
 
-    #se lee el archivo csv
-    datos = pd.read_csv(archivo, names=label, header=0)
+    #se crea una lista con los nombres de archivos del directorio indicado
+    lista = ob.obtener_archivos(archivo, fecha)
 
-    #Se genera una lista vacía de vectores
-    listVect = []
-
-    #creación de vector de tiempo
-    tiempo = datos.index * time
-    #time es el tiempo de muestreo en segundos
-
-    #iteración
-    for name in label:
-        listVect.append(datos[name].values)
-
-    #se crea un diccionario para acceder a los vectores
-    dicVect = dict(zip(label,listVect))
-    if values:
-        return dicVect
-    else:
-        return tiempo
-
-def extraerValor(nombre):
-    dicc=extraerData(file)
-    return dicc[nombre]
-
-def extraerTiempo():
-    tiempo = extraerData(file, values=False)
-    return tiempo
-
-tiempo = extraerTiempo()
-busVoltage = extraerValor('busVoltage')
-#plt.plot(tiempo,busVoltage)
-busCurrent = extraerValor('busCurrent')
-#plt.plot(tiempo,busCurrent)
-#plt.show()
+    #se leen los archivos csv creandose una lista de tablas
+    tablas = []
+    for x in archivos:
+        datos = pd.read_csv(x, names=label, header=0,)
+        tablas.append(datos)
+    return tablas
