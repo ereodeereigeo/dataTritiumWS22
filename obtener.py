df592acecf22fbbbb1b4123bf59a04f1b9ec279b
Rodrigo Lara <ereodeereigeo@gmail.com>
2015-01-31 02:31:11 -0300

    Se añadió función lista de archivos la cual extrae el nombre de los archivos los enlista y ordena

--- obtener.py
+++ obtener.py
@@ -1,6 +1,16 @@
 # -*- coding: utf-8 -*-
 import os
+import glob
 
+def lista_archivos():
+    
+    lista = glob.glob('*.csv')
+    for i in xrange(0,len(lista)-1):
+        if not 'WSlog' in lista[i]:
+            del lista[i]
+        else:
+            return lista
+        
 def obtener_archivos(directorio , fecha=None):
 
     #obtenemos el path completo actual
@@ -17,21 +27,26 @@ def obtener_archivos(directorio , fecha=None):
         lista_dir_act = [directorio]
         
     #verificamos si los archivos están en el mismo directorio del módulo
-    if directorio == lista_dir_act[-1]: 
-        return 'está'
+    if directorio == lista_dir_act[-1]:
+
+        #añadimos los nombres de los archivos a una lista
+        nombres_archivos = lista_archivos()
+        nombres_archivos.sort()
+        return nombres_archivos
 
     #si no, probamos si está en una carpeta dentro del directorio actual
     elif os.path.exists(directorio): 
 
         os.chdir(directorio) #y si está entramos a esa carpeta
 
-        return os.getcwd()
+        #y añadiremos los nombres de los archivos a una lista
+        nombres_archivos = lista_archivos()
+        nombres_archivos.sort()
+        return nombres_archivos
+        
     else:
         return '''Error, ingrese el directorio completo:
                 
     linux: '/home/user/.../carpeta' 
                 
-    windows: 'C:\Usuario\Escritorio\Carpeta' '''
-
-        
-        
+    windows: 'C:\Usuario\Escritorio\Carpeta' '''        
