654868ba74a0401878f6ee31ebd450d73fcca22e
Rodrigo Lara <ereodeereigeo@gmail.com>
2015-02-03 01:23:30 -0300

    Reindexado completado

--- ext_datos.py
+++ ext_datos.py
@@ -23,14 +23,19 @@ def extraer_data(archivo, fecha=None, label=listaEnc, time=0.2, values=True):
 
         #lee los datos del archivo csv
         datos = pd.read_csv(nombres, names=label, header=0, index_col = 0, parse_dates = True)
-        #fecha_indice_inf = nombres[6:16]+' '+datos['time'].head(1).values[0]
-        #fecha_indice_sup = nombres[6:16]+' '+datos['time'].tail(1).values[0]
-        #serie_tiempo = pd.date_range(fecha_indice_inf, fecha_indice_sup, freq='200ms')
-
+        fecha_indice_inf = nombres[6:16]+' '+nombres[17:19]+':'+nombres[20:22]+':'+nombres[23:25]
+        periodo = len(datos.index)
+        tiempo_nuevo=pd.date_range(fecha_indice_inf,periods=periodo, freq='200ms', name = 'time')
+        dataFrame = pd.DataFrame(datos.values, index = tiempo_nuevo, columns =['errors', 'limiters', 'currentSP', \
+        'velocitySP', 'idcSP', 'miscSP', 'busVoltage','busCurrent',\
+        'velocityMs', 'motorRpm', 'pcCurrent', 'pbCurrent', 'odometer',\
+        'busCharge', 'bemf','voutD', 'voutQ', 'ioutD', 'ioutQ', '15v',\
+        '1.9v', '3.3v', 'motorTemp', 'dspTemp', 'phaseaTemp', 'phasebTemp',\
+        'phasecTemp', 'cantranserr', 'canrecerr', 'slipSpeed'] )
         if fecha == None:
-            tablas.append(datos)
+            tablas.append(dataFrame)
         elif str(fecha) == str(nombres[6:16]):
-            tablas.append(datos)
+            tablas.append(dataFrame)
 
     return tablas
 
